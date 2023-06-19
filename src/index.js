d25d162625b76b0ec74e2df04754ec64801d92a2
NataliFlego <natali.flego6@gmail.com>
2023-06-19 12:19:14 +0200

    cross headers

--- src/index.js
+++ src/index.js
@@ -16,6 +16,33 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+// Set middleware of CORS 
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "http://127.0.0.1:5173"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
+
+
+
 app.get('/', (req, res) => { })
 
 // vrć middleware funkcije na rute [auth.verify]
@@ -219,6 +246,8 @@ app.put('/:commentId', [auth.verify], async (req, res) => {
 
 // pretraga bolesti
 app.get('/upisibolest', async (req, res) => {
+
+    console.log('Request received for /upisibolest');
     let db = await connect("bolesti");
     let term = req.query.term;
     let reg = `/${term}/`;
