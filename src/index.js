8f92151162dee93a2e6dfacecdb7fdfea43938e7
NataliFlego <natali.flego6@gmail.com>
2023-06-20 13:28:21 +0200

    sa opcijama

--- src/index.js
+++ src/index.js
@@ -12,12 +12,11 @@ import auth from './auth';
 const app = express()
 const port = 4000 || process.env.PORT
 
-/* const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-} */
-/* app.use(cors()); */
+const corsOptions = {
+    origin: 'https://bolestizdravlje.onrender.com',
+
+}
+app.use(cors(corsOptions));
 
 
 app.use(cors(/* corsOptions */)) // Use this after the variable declaration
