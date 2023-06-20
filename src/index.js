5f8714ce38e14f6f016d7cdce2c356595cadfaac
NataliFlego <natali.flego6@gmail.com>
2023-06-20 12:41:54 +0200

    bez headera

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Set middleware of CORS 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Max-Age", 7200);
 
     next();
-});
+}); */
 
 
 
